1d063a5ca5fb8c40f19f4be08c866d52447291d3
GabrielDuarteMg <gabriel160499@gmail.com>
2020-01-06 00:27:32 -0300

    Cors Fixed On Server

--- js/main.js
+++ js/main.js
@@ -1,74 +1,83 @@
 $(document).ready(event => {
-  var validEmail = email => {
-    return new Promise((resolve, reject) => {
-      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      if (re.test(String(email).toLowerCase())) {
-        resolve(true);
-        return true;
-      } else {
-        resolve(false);
-      }
-    });
-  };
-  testInput = event =>
-    new RegExp(/[a-zåäö ]/i).test(String.fromCharCode(event.which));
-  $("#nameInput").bind("keypress", testInput);
-
-  $("#sendBtn").click(() => {
-    //Validation key Email f3eb15399a404281aa1b788b5cdc831c
-    if (
-      $("#nameInput").val() == "" ||
-      $("#nameInput").val() == null ||
-      $("#nameInput").val() == undefined
-    ) {
-      Notify("Please insert valid name.", null, null, "warning");
-      return;
-    }
-    if (
-      $("#form7").val() == "" ||
-      $("#form7").val() == null ||
-      $("#form7").val() == undefined
-    ) {
-      Notify("Please insert message.", null, null, "warning");
-      return;
-    }
-    var response = grecaptcha.getResponse();
-    if (response.length == 0) {
-      Notify("Captcha error", null, null, "warning");
-      return;
-    } else {
-      if (
-        $("#emailInput").val() != "" ||
-        $("#emailInput").val() != null ||
-        $("#emailInput").val() != undefined
-      ) {
-        validEmail($("#emailInput").val()).then(res => {
-          if (res == true) {
-            let bodyRequest = JSON.stringify({
-              subject: "Contato - Portifolio",
-              emailContact: $("#emailInput").val(),
-              text: `${$("#nameInput").val()}\n\n${$("#form7").val()}`
-            });
-            fetch("https://fastmailservice.herokuapp.com/", {
-              method: "POST",
-              mode: "cors",
-              headers: {
-                "Content-Type": "application/json"
-              },
-              body: bodyRequest
-            })
-              .then(response => {
-                return response;
-              })
-              .then(data => {
-                console.log(data);
-              })
-              .then(error => console.log(error));
-          } else {
-            Notify("Please insert valid email", null, null, "warning");
-          }
+    var validEmail = email => {
+        return new Promise((resolve, reject) => {
+            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            if (re.test(String(email).toLowerCase())) {
+                resolve(true);
+                return true;
+            } else {
+                resolve(false);
+            }
         });
-      }
-    }
-  });
+    };
+    testInput = event =>
+        new RegExp(/[a-zåäö ]/i).test(String.fromCharCode(event.which));
+    $("#nameInput").bind("keypress", testInput);
+
+    $("#sendBtn").click(() => {
+        //Validation key Email f3eb15399a404281aa1b788b5cdc831c
+        if (
+            $("#nameInput").val() == "" ||
+            $("#nameInput").val() == null ||
+            $("#nameInput").val() == undefined
+        ) {
+            Notify("Please insert valid name.", null, null, "warning");
+            return;
+        }
+        if (
+            $("#form7").val() == "" ||
+            $("#form7").val() == null ||
+            $("#form7").val() == undefined
+        ) {
+            Notify("Please insert message.", null, null, "warning");
+            return;
+        }
+        var response = grecaptcha.getResponse();
+        if (response.length == 0) {
+            Notify("Captcha error", null, null, "warning");
+            return;
+        } else {
+            if (
+                $("#emailInput").val() != "" ||
+                $("#emailInput").val() != null ||
+                $("#emailInput").val() != undefined
+            ) {
+                validEmail($("#emailInput").val()).then(res => {
+                    if (res == true) {
+                        let bodyRequest = JSON.stringify({
+                            subject: "Contato - Portifolio",
+                            emailContact: $("#emailInput").val(),
+                            text: `${$("#nameInput").val()}\n\n${$("#form7").val()}`
+                        });
+                        fetch("https://fastmailservice.herokuapp.com/", {
+                                method: "POST",
+                                headers: {
+                                    "Content-Type": "text/plain",
+                                    'Access-Control-Allow-Headers': '*',
+                                    'Access-Control-Allow-Origin': '*',
+                                    'Access-Control-Request-Method': 'POST',
+                                    'Access-Control-Allow-Headers': 'Content-Type'
+                                },
+                                body: bodyRequest
+                            })
+                            .then(response => {
+                                console.log(response);
+                            })
+                            .then(data => {
+                                console.log(data);
+                            })
+                    } else {
+                        Notify("Please insert valid email", null, null, "warning");
+                    }
+                });
+            }
+        }
+    });
+});
+
+
+let bodyRequest = JSON.stringify({
+    subject: "Contato - Portifolio",
+    emailContact: $("#emailInput").val(),
+    text: `${$("#nameInput").val()}\n\n${$("#form7").val()}`
 });
