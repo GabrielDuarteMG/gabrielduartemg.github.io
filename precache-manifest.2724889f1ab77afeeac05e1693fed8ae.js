3ae6466a88e2ef5c67189abea02a92bdab5e478e
Gabriel Duarte <gdsantos@unimeduberlandia.coop.br>
2020-09-18 14:34:54 -0300

    Updates

---
+++ precache-manifest.2724889f1ab77afeeac05e1693fed8ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ceb85fc846e21a32a2cc",
+    "url": "/portfolio/static/css/main.25490c3b.chunk.css"
+  },
+  {
+    "revision": "ceb85fc846e21a32a2cc",
+    "url": "/portfolio/static/js/main.ceb85fc8.chunk.js"
+  },
+  {
+    "revision": "2856f85b658599dbd182",
+    "url": "/portfolio/static/css/1.7f73b6a3.chunk.css"
+  },
+  {
+    "revision": "2856f85b658599dbd182",
+    "url": "/portfolio/static/js/1.2856f85b.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "65b8de4780fe39e6e32f8020b7ac35b7",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2d0d78d8a3f0cda2241b3de5b75f56bd.js"
+  "/portfolio/precache-manifest.2724889f1ab77afeeac05e1693fed8ae.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
